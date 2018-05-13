c7dac7ec3a29188ffa16a5582175ca418f40348d
delbertbeta <delbertbeta@live.com>
2018-05-13 14:20:28 +0800

    Added Download API.

---
+++ routes/download.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const homeworkList = require('../tools/sequelize')
+const zipFolder = require('zip-folder')
+const path = require('path')
+const fs = require('fs')
+
+const router = express.Router();
+
+router.get('/api/download', (req, res) => {
+  homeworkList.findOne({
+    where: {
+      id: req.query.id
+    }
+  }).then(i => {
+    if (fs.existsSync(path.resolve('./upload/', i.name + '.zip'))) {
+      fs.unlinkSync(path.resolve('./upload/', i.name + '.zip'));
+    }
+    zipFolder(path.resolve('./upload/', i.name), path.resolve('./upload/', i.name + '.zip'), function(err) {
+      if(err) {
+          res.send('oh no!', err);
+      } else {
+          res.sendFile(path.resolve('./upload/' + i.name + '.zip'))
+      }
+    })
+  })
+})
+
+module.exports = router;
